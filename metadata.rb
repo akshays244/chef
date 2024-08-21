39cd0c03423e9825b16aaa2c5f2e995f4ce8864b
Akshay <akshays244@gmail.com>
2024-08-21 22:46:21 +0530

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+# metadata.rb
+
+name 'nginx_install'
+maintainer 'Your Name'
+maintainer_email 'your_email@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures nginx'
+version '0.1.0'
+chef_version '>= 14.0'
